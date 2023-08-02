149d1349ede02a4898573121c73620dd6df47785
jemicom <jemicom@gmail.com>
2023-08-02 19:16:45 +0900

    coins, pagination add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { useContext} from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React, { useContext, useState , useEffect} from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import './App.css'
 import {
   Header, 
-  Footer, 
-  Logo,
+  Footer,  
   NavBar
 } from './components'
 
 
-import { ThemeContext } from './contexts/ThemeContext'
 import { AuthContext } from './contexts/AuthContext'
 
 import {
@@ -19,12 +17,16 @@ import {
   Posts, 
   PostDetail,
   Login,
-  NotFound
+  NotFound,
+  NewPost
 } from './routes'
 
+import { format  } from 'date-fns';
+import PostLayout from './layouts/PostLayout'
+
 const App = () => {
-  const { darkmode }= useContext(ThemeContext)
-  const { user }= useContext(AuthContext)
+  const { user }= useContext(AuthContext);
+  
   return (
     <>
       <Header />
@@ -32,10 +34,17 @@ const App = () => {
       <Routes>
           <Route path="/"  element={<Main />} />
           <Route path="/main"  element={<Main />} />
+
           <Route path="/coins"  element={<Coins />} />
-          <Route path="/coins/coindetail"  element={<CoinDetail />} />
-          <Route path="/posts"  element={<Posts />} />
-          <Route path="/posts/postdetail"  element={<PostDetail />} />
+          <Route path="/coins/:id"  element={<CoinDetail />} />
+
+          <Route path='/posts'  relative="path"  element={ <PostLayout   /> } >
+              <Route index  element={ <Posts   />} />
+              <Route path=":id"  element={ <PostDetail  />} />
+              <Route path="newpost"  element={ <NewPost  />} />
+          </Route>
+          {/* PostLayout end  */}
+
           <Route path="/login"  element={<Login />} />
           <Route path="/*"  element={<NotFound />} />
       </Routes>
@@ -49,3 +58,6 @@ export default App
 
 // git repository 만들고
 // axios, browserRoute, motions, wrapperComponent,  coins, board, CRUD, NODE, todos
+
+// 여러분들 것도 배포하기 : 주말에 URL 업로드하기 
+// https://web-frontend-07-7xwyjq992llivdr0fu.sel4.cloudtype.app/
